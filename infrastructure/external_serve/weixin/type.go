916804e9757fd83ec4a6d86401006a5559bf4842
Patric Peng <patric.peng@aladinbank.id>
2023-11-28 22:35:34 +0800

    fix CI/CD

--- infrastructure/external_serve/weixin/type.go
+++ infrastructure/external_serve/weixin/type.go
@@ -2,6 +2,19 @@ package externalAPI
 
 import "github.com/go-resty/resty/v2"
 
+const (
+	VerifyStudentStatusURL = "https://www.chsi.com.cn/xlcx/bg.do?vcode=%s&srcid=bgcx"
+)
+
+type XueXinProvider struct {
+	client *resty.Client
+}
+
+type StudentInfo struct {
+	Name string
+	Sex  string
+}
+
 type WeiXinProvider struct {
 	client *resty.Client
 }
